7c80ede17cbef91e794ec7e8cd4e1b959c97c02b
Laura Espiño <126553407+LauraEspino@users.noreply.github.com>
2023-06-20 19:05:13 +0000

    diseño semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,13 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center semaforo">
+			<div className="sostenSemaforo"></div>
+			<div  className="fondoSemaforo">
+					<div className="luzRoja"></div>
+					<div className="luzAmarilla"></div>
+					<div className="luzVerde"></div>
+			</div>
 		</div>
 	);
 };
