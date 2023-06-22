aa84f5c1f76b7b0e0a5d6204226adb2d26d9c62a
Laura Espiño <126553407+LauraEspino@users.noreply.github.com>
2023-06-22 06:52:44 +0000

    trafficlight finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	const [glow,setGlow] = useState("")
+
 	return (
 		<div className="text-center semaforo">
 			<div className="sostenSemaforo"></div>
 			<div  className="fondoSemaforo">
-					<div className="luzRoja"></div>
-					<div className="luzAmarilla"></div>
-					<div className="luzVerde"></div>
+				<div className={`red ${glow === "red" ? "glow" : ""}`} 
+					onClick={() => {setGlow("red")}}>
+				</div>
+				<div className={`yellow ${glow === "yellow" ? "glow" : ""}`} 
+						onClick={() => {setGlow("yellow")}}>
+				</div>
+				<div className={`green ${glow === "green" ? "glow" : ""}`} 
+						onClick={() => {setGlow("green")}}>
+				</div>
 			</div>
 		</div>
 	);
